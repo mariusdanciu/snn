6d507b7af987ca85f50e90278ed900f457790006
mariusdanciu <marius.danciu@ro.ibm.com>
2019-11-11 14:36:42 +0200

    update

--- src/neunet/definitions.rs
+++ src/neunet/definitions.rs
@@ -15,23 +15,29 @@ impl MLOps {
 
     pub fn loss(&self, y: f64, w: &Array1<f64>, x: &Array1<f64>, b: f64) -> f64 {
         let y_hat = self.sigmoid(self.hypothesis(w, x, b));
-        - (y * y_hat.ln() + (1. - y)*(1. - y_hat).ln())
+        -(y * y_hat.ln() + (1. - y) * (1. - y_hat).ln())
     }
 
     pub fn loss_from_pred(&self, y: f64, y_hat: f64) -> f64 {
-        - (y * y_hat.ln() + (1. - y)*(1. - y_hat).ln())
+        -(y * y_hat.ln() + (1. - y) * (1. - y_hat).ln())
     }
+}
+
+pub enum OptimizationType {
+    stochastic_gradient_descent,
+    batch_gradient_descent,
+    adam,
 }
 
 pub enum ActivationType {
     sigmoid,
     relu,
-    soft_max
+    soft_max,
 }
 
 pub struct Layer {
     pub num_activations: i32,
-    pub activation_type: ActivationType
+    pub activation_type: ActivationType,
 }
 
 pub struct NeuralNetwork {
